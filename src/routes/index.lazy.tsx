2b43b459332d3db6f7930cd0e3efec5c5ceac3bf
Ben Harrington <benmharrington@gmail.com>
2025-01-09 18:32:31 -0800

    installed tanstack router

---
+++ src/routes/index.lazy.tsx
@@ -0,0 +1,15 @@
+import { createLazyFileRoute } from '@tanstack/react-router'
+
+export const Route = createLazyFileRoute('/')({
+  component: Index,
+})
+
+function Index() {
+
+  console.log(import.meta.env.MODE);
+  return (
+    <div className="p-2">
+      <h3>Welcome Home!</h3>
+    </div>
+  )
+}
